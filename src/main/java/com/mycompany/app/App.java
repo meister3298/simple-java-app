6c6bf4349a5fd291f08b2bbcad6aa7f4490d44dd
Chong Wooi <chongwooi@gmail.com>
2026-04-07 13:38:28 +0800

    Make a change to pipeline job to demonstrate web hook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Make further changes for pipeline from local project !");
+        System.out.println("Make changes to demonstrate web hook for pipeline from local project !");
     }
 
     public String getMessage() {
