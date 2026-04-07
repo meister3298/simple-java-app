de6a4d3f9e8dd6dbc057b5fae9f6c0c71e3c3a5c
Chong Wooi <chongwooi@gmail.com>
2026-04-07 13:34:07 +0800

    Make a change to pipeline job to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Some changes for pipeline from local project !");
+        System.out.println("Make further changes for pipeline from local project !");
     }
 
     public String getMessage() {
